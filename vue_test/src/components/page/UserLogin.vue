511985fc0806d90aa4df448b8a2a34a925a947f5
zyzhang <zhangtaihu101@163.com>
2017-04-17 22:54:36 +0800

    vue基础知识

---
+++ vue_test/src/components/page/UserLogin.vue
@@ -0,0 +1,51 @@
+<template>
+    <div id="user-login" class="col-md-8 col-md-offset-2">
+        <h2 class="text-center">用户登录</h2>
+        <form class="form-horizontal" role="form">
+            <user-name ref="uname" v-on:updateUserName="setUserName" placeholder_z="请输入用户名"></user-name>
+            <user-pass ref="upass"></user-pass>
+            <user-area v-on:childChange="setValue"></user-area>
+            <user-submit></user-submit>
+        </form>
+    </div>
+</template>
+
+<style>
+    @import "../../../src/css/bootstrap.min.css";
+
+    #user-login {
+        margin-top: 50px;
+    }
+</style>
+
+<script>
+    import username from './user/UserName.vue'
+    import userpass from './user/UserPass.vue'
+    import userarea from './user/UserArea.vue'
+    import usersubmit from './user/UserSubmit.vue'
+
+    export default{
+        data(){
+            return {
+                username: "",
+                userpass: "",
+                index:""
+            }
+        },
+        methods: {
+            setUserName(uname){
+                this.username = uname;
+            },
+            setValue(key, index){
+                console.log(key, index);
+                this.index = index;
+            }
+        },
+        components: {
+            "user-name": username,
+            "user-pass": userpass,
+            "user-area": userarea,
+            "user-submit": usersubmit,
+        }
+    }
+</script>
---
+++ vue_test/src/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by a1 on 17/4/17.
+ */
+import Vue from 'vue'
+import userlogin from '../src/components/page/UserLogin.vue'
+
+new Vue({
+    el: "#app",
+    components: {"user-login": userlogin}
+})
